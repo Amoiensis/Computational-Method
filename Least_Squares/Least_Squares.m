b55e7d3f9bacf00c0723c7e6ae3186e08fcba0e6
Amoiensis <yxp189@portonmail.com>
2019-10-22 17:12:20 +0800

    Update Least_Squares.m

--- Least_Squares/Least_Squares.m
+++ Least_Squares/Least_Squares.m
@@ -1,45 +1,63 @@
 %%
 % Author: Amoiensis
-% Date: 2019.10.15
-% Homework 10.15
-% Experiment: #2 -- fit w=c*t^lamda
+% Date: 2019.10.22
+% Course: Least_Squares(最小二乘法+可实现多项式拟合)
 %%
 % import data
-% w=c*t^lamda
-t = [1 2 4 8 16 32 64];
-w = [4.22 4.02 3.85 3.59 3.44 3.02 2.59];
-% 用于线性拟合的变量取值
-t_n = log(t);%[36.9 46.7 63.7 77.8 84.0 87.5];
-w_n = log(w);%[181 197 235 270 283 292];
+clear;clc;
+t_n = [19   25  31  38  44];
+w_n = [19.0 32.3    49.0    73.3    97.8];
+time = 1;   %使用拟合的多项式次数
 Size = size(t_n);
 
-L = [...
-    Size(2) sum(t_n);...
-    sum(t_n) sum(t_n.^2);...
-    ];
-R = [...
-    sum(w_n);
-    sum(w_n.*t_n);
-    ];
-Coef = (L)^(-1)*R;
+% 右端R矩阵
+for i=1:(time+1)
+    % 右端R矩阵
+    for j=1:(time+1)
+        if (i+j)~=2
+            L(i,j)= sum(t_n.^(i+j-2));
+        else
+            L(i,j) = Size(2) ;
+        end
+    end
+end
+
+% 左端L矩阵
+for i=1:(time+1)
+    % 左端L矩阵
+    if i~=1
+        R(i,:) = sum((t_n.^(i-1)).*w_n);
+    else
+        R(i,:) = sum(w_n);
+    end
+end
+
+Coef = inv(L)*R;
 %%
-%Plot Liner
+% Plot
 plot (t_n,w_n,'r*');
-x = 1:0.01:7;
+x = min(t_n):0.01:max(t_n);
+for i=1:(time+1)
+    % 左端L矩阵
+    X(i,:) = x.^(i-1);
+end
 hold on;
-fit_y = x.*Coef(2)+Coef(1);
-plot (x,fit_y);
+fit_y = (Coef')*X;
+plot (x,fit_y,'r');
 title('线性拟合结果');
 xlabel('x');
 ylabel('liner_y');
-
-%Plot origin
-figure();
-plot (t,w,'r*');
-x = 1:0.01:5;
-fit_y = exp(x.*Coef(2)+Coef(1));
-hold on;
-plot (exp(x),fit_y);
-title('原函数拟合结果');
-xlabel('x');
-ylabel('y');
+%%
+% Caculate Gap
+for i=1:(time+1)
+    % 左端L矩阵
+    T_n(i,:) = t_n.^(i-1);
+end
+fit_w_value  = (Coef')*T_n;
+abs_gap = abs(fit_w_value - w_n);
+[abs_gap_max] = max(abs_gap);
+disp('abs_gap_max');
+disp(abs_gap_max);
+ave_square_gap = sum(abs_gap.^2)/Size(2);
+disp('ave_square_gap');
+disp(ave_square_gap);
